c1ea9686535e9aa1165df20e33c14a660ae41131
Mohamed Youssef Chetoui <mchetoui@c4r3p6.1337.ma>
2024-11-01 18:39:27 +0100

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mchetoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 00:09:36 by mchetoui          #+#    #+#             */
-/*   Updated: 2024/11/01 17:08:58 by mchetoui         ###   ########.fr       */
+/*   Updated: 2024/11/01 18:39:18 by mchetoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,27 @@ int check(char c, char const *set)
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *new;
-	char *str;
-	int len;
 	int i;
+	int len;
+	char *new;
 
-	if (!set || !*set || !s1 || !*s1)
-		return ((char *)s1);
 	len = ft_strlen(s1);
-	str = (char *)s1;
-	while (*str)
-		if (check(*(str++), set))
-			len--;
-	new = malloc(len + 1);
+	while (--len && check(s1[len], set))
+		;	
+	if (!len)
+		return(malloc(0));
+	i = -1;
+	while (s1[++i] && check(s1[i], set))
+		len--;
+	new = malloc(++len + 1);
 	if (!new)
 		return (NULL);
-	str = (char *)s1;
-	i = 0;
-	while (*str)
-		if (!check(*str, set))
-			new[i++] = *(str++);
-	new[i] = 0;
-	return (new);
-}
-
-int main()
-{
-	printf("%s\n", ft_strtrim("lhrpa app wlh", "wal"));
+	ft_memset(new, 'a', len);
+	new[len] = 0;
+	while (*new)
+	{
+		*new = s1[i++];
+		new++;
+	}
+	return (new - len);
 }
