3b4ce3a1ba35d9b092735f3cf0d8c564522f9c26
Mohamed Youssef Chetoui <mchetoui@c4r10p5.1337.ma>
2024-11-03 04:04:54 +0100

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mchetoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 23:06:45 by mchetoui          #+#    #+#             */
-/*   Updated: 2024/11/03 02:38:01 by mchetoui         ###   ########.fr       */
+/*   Updated: 2024/11/03 04:04:49 by mchetoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ char	*ft_strdup(char *src)
 	int		i;
 	char	*new;
 
-	if (!src)
-		return (NULL);
 	new = malloc((ft_strlen(src) + 1) * sizeof(char));
-	i = 0;
-	while (src[i])
-	{
+	if (!new || !src)
+		return (NULL);
+	i = -1;
+	while (src[++i])
 		new[i] = src[i];
-		i++;
-	}
 	new[i] = '\0';
 	return (new);
 }
